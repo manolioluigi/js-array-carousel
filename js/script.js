2231517c845724dba277fc8b8b042f624fb4c0ed
manolioluigi <luigi.manolio1999@gmail.com>
2022-12-01 17:49:41 +0100

    esercizio + bonus 1

---
+++ js/script.js
@@ -0,0 +1,81 @@
+//Creo array immagini
+const imagesArray = [
+    "01.webp",
+    "02.webp",
+    "03.webp",
+    "04.webp",
+    "05.webp"
+]
+
+//Creiamo dinamicamente i div con le immagini del carosello
+let itemsContent = '';
+
+for(let i = 0; i < imagesArray.length; i++){
+    itemsContent += `<div class="item">
+        <img src="./img/${imagesArray[i]}">
+    </div>`
+}
+
+//inseriamo le immagini nel div che le deve contenere
+const itemsSlider = document.querySelector('.item-slider');
+itemsSlider.innerHTML += itemsContent;
+
+//Prendiamo la prima immagine dell'array e la rendiamo attiva
+
+//const items = document.querySelector('.item'); //ALTERNATIVA
+
+const items = document.getElementsByClassName('item');
+let itemActive = 0;
+
+items[itemActive].classList.add('active');
+
+//rendo attivo anche il primo cerchio di navigazione
+
+const circles = document.getElementsByClassName('circle');
+
+circles[itemActive].classList.add('active');
+
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+next.addEventListener('click', function(){
+    circles[itemActive].classList.remove('active');
+    items[itemActive].classList.remove('active');
+
+    if(itemActive+1 == imagesArray.length){
+        
+        itemActive = 0;
+        
+        
+    } else {   
+        itemActive++;
+    }
+
+    items[itemActive].classList.add('active');
+    circles[itemActive].classList.add('active');
+
+
+});
+
+prev.addEventListener('click', function(){
+
+    circles[itemActive].classList.remove('active');
+    items[itemActive].classList.remove('active');
+
+    if(itemActive-1 == -1){
+        
+        itemActive = imagesArray.length-1;
+
+    } else {
+        
+        itemActive--;
+        
+    }
+
+    items[itemActive].classList.add('active');
+    circles[itemActive].classList.add('active');
+
+
+})
+
+
